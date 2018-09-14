4c13bb875d1339894ae4018861cfff14298e17f6
Eshaffer <edmond.shaffer@gmail.com>
2018-09-13 23:03:04 -0400

    changes

--- code/BamazonCustomer.js
+++ code/BamazonCustomer.js
@@ -15,63 +15,73 @@ var connection = mysql.createConnection({
   database: "Bamazon"
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
+connection.connect(function (err) {
+  if (err) throw err;
   displayProducts();
+});
+
+function displayProducts() {
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+
+    var table = new Table({
+      head: ['ITEM ID', 'NAME', 'PRICE', 'STOCK']
+      , colWidths: [10, 40, 10, 10]
+    });
+    for (var i = 0; i < res.length; i++) {
+      table.push(
+        [res[i].id, res[i].product, res[i].price, + res[i].quantity]
+      );
+    }
+    console.log(table.toString());
+
+    start();
+
   });
 
-  function displayProducts() {
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-     
-      var table = new Table({
-        head: ['ITEM ID', 'NAME','PRICE']
-      , colWidths: [10,40,10]
-    });
-for(var i=0;i < res.length;i++){
-  table.push(
-    [res[i].id,res[i].product,res[i].price]
-);
 }
-console.log(table.toString());
-    
-      start();
-    });
-   
-  }
 
- var start= function () {
-    inquirer
-      .prompt([
-        {
-        name: 'id',
+var start = function () {
+  inquirer
+    .prompt([
+      {
+
+        name: 'pickedId',
         type: 'input',
         message: 'Which item would you like to purchase? (Enter the Product ID)'
-      }
-  ]).then(function(answer) {
-    console.log(answer)
-    console.log("SELECT product FROM PRODUCTS WHERE ID="+answer.id);
-    connection.query("SELECT product FROM products WHERE ID="+answer.id,function(err,res){
-      if (err) throw err;
-      console.log(res[0].product);
-      inquirer
-      .prompt([
-        {
+      },
+      {
         name: 'qty',
-        type: 'input',
-        message: 'how many '+res[0].product+" would you like to buy"
+        message: 'how many  would you like to buy?',
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
       }
-  ]).then(function(answer) {
-    console.log(answer.qty)
-  })
-    });
-   
-      // get the information of the chosen item
-      // var chosenItem;
-      // for (var i = 0; i < results.length; i++) {
-      //   if (results[i].item_name === answer.choice) {
-      //     chosenItem = results[i];
-      //   }
-      //}
+    ]).then(function (answer) {
+      // console.log(answer)
+      // console.log("SELECT product FROM PRODUCTS WHERE ID=" + answer.id);
+      connection.query("SELECT * FROM products WHERE ID ?=" + answer.pickedId, function (err, res) {
+
+        console.log("~/~/~/ Processing /~/~/~")
+        console.log(answer);
+        // console.log(
+        //   "Product: " +
+        //   res[0].product
+        // );
+        for (var j = 0; j < res.length; j++) {
+          if (answer.qty > res[0].quantity) {
+            console.log("low on stock")
+          };
+
+        }
+      })
+
+
     })
-  };
+
+};
+
+
